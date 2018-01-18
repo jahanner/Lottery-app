1c85e2a7ec01d34e5903eeb3a1d8cd2098ec7050
Jesse Hanner <j-money@Jesses-MacBook-Pro.local>
2018-01-18 16:51:46 -0600

    added a second page copy to easily modify

---
+++ src/page-two/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import logo from '../images/logo-circle.png';
+import EntryForm from './entry-form.js';
+import Clock from './countdown-timer.js';
+import '../styles/App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {deadline: 'January, 20, 2018'};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className='title/odds'>
+            <h1 className="App-title">Welcome to Lottery App</h1>
+            <h3>Your odds of winning are: 1:1</h3>
+          </div>
+          <div className='Clock'>
+            <Clock deadline={this.state.deadline}/>
+          </div>
+          </header>
+        </div>
+        <div>
+          <h2 className="Prize-Description">Prize Description Here...</h2>
+          <p className="App-intro">
+          To get started, enter your info.
+          </p>
+        </div>
+        <EntryForm />
+      </div>
+    );
+  }
+}
+
+export default App;
